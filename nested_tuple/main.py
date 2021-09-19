ef44b08c44a2cf4b2d6b19dcad80bee51cdf501b
0xturazzi <65428925+0xturazzi@users.noreply.github.com>
2021-09-19 13:27:55 -0300

    Update main.py

--- nested_tuple/main.py
+++ nested_tuple/main.py
@@ -3,19 +3,22 @@ from midiutil.MidiFile import MIDIFile
 
 # Author: https://github.com/0xturazzi
 
+# Having both notes and note.subs is redundant
+# But it will be useful for note deletion if I decide to make a GUI
+
 class Note:
     def __init__(self, time, duration):
-        self.sub = []
+        self.sub = [] # keeps track of the index of it's child notes on the main notes array
         self.time = time
         self.duration = duration
 
     def divide(self, divs):
         global notes
-        self.sub = []
+        self.sub = [] # clearing it will allow interactively changing the tuple on the UI w/o trouble
         time = self.time
-        duration = self.duration / divs
+        duration = self.duration / divs # if needed, precision can be increased using decimal
         for i in range(divs):
-            self.sub.append(len(notes))
+            self.sub.append(len(notes)) 
             notes.append(Note(time, duration))
             time += duration
 
@@ -31,18 +34,19 @@ def save(notes):
     volume = 100
 
     for note in notes: 
-        if note.sub: continue
+        if note.sub: continue # Prevent Overlap from parent note
         
         time = note.time
         duration = note.duration
         mf.addNote(track, channel, pitch, time, duration, volume)
+
     with open(f"{name}.mid", 'wb') as outfile:
         mf.writeFile(outfile)
 
 ##########################################################
 # Config
 name = "GeneratedTuples"
-start_size = 4 # In Beats
+start_size = 4 # Measured in Beats
 notes = [Note(0, start_size)]
 ##########################################################
 # Edit Area
